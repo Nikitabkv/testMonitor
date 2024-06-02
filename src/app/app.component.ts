4ca1278c54d13d33404089fdbb331981e7891c83
n.bakumenko <n.bakumenko@numedy.com>
2024-06-02 12:38:22 +0300

    feat: section divider moving

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./header/header.component";
 import {SubstationsComponent} from "./substations/substations.component";
@@ -14,4 +14,43 @@ import {FooterComponent} from "./footer/footer.component";
 })
 export class AppComponent {
   title = 'testMonitor';
+
+  section1Width = 500;
+  section2Width = 500;
+  isResizing = false;
+  startX = 0;
+  startWidth = 0;
+  nextWidth = 0;
+
+  @HostListener('mousedown', ['$event'])
+  @HostListener('touchstart', ['$event'])
+  startResize(event: MouseEvent | TouchEvent) {
+    document.body.style.userSelect = 'none';
+    this.isResizing = true;
+    this.startX = (event as TouchEvent).touches ? (event as TouchEvent).touches[0].pageX : (event as MouseEvent).pageX;
+    this.startWidth = this.section1Width;
+    this.nextWidth = this.section2Width;
+  }
+
+  @HostListener('mousemove', ['$event'])
+  @HostListener('touchmove', ['$event'])
+  resize(event: MouseEvent | TouchEvent) {
+    if (this.isResizing) {
+      const offsetX = (event as TouchEvent).touches ? (event as TouchEvent).touches[0].pageX - this.startX : (event as MouseEvent).pageX - this.startX;
+      const newWidth = this.startWidth + offsetX;
+      const newNextWidth = this.nextWidth - offsetX;
+
+      if (newWidth > 0 && newNextWidth > 0) {
+        this.section1Width = newWidth;
+        this.section2Width = newNextWidth;
+      }
+    }
+  }
+
+  @HostListener('mouseup', ['$event'])
+  @HostListener('touchend', ['$event'])
+  endResize(event: MouseEvent | TouchEvent) {
+    this.isResizing = false;
+    document.body.style.userSelect = 'auto';
+  }
 }
